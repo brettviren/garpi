f8fb3d3635da3dff5ebabb719c64c50c92a1ee09
Brett Viren <bviren@lycastus.phy.bnl.gov>
2009-07-28 13:54:31 -0400

    Basic HTTP get working.

---
+++ python/garpi/garpi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class Main:
+    def __init__(*args):
+        from command import Options
+        import sys
+        self.opts = Options(args)
+        return
+
+    
+
+    
---
+++ python/garpi/get.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+'''
+Classes and functions to get files or directory trees from
+repositories.
+'''
+
+def get_http_ftp(what,url,target,overwrite):
+    from urllib2 import Request, urlopen, URLError, HTTPError, ProxyHandler, build_opener, install_opener
+    import os, shutil
+
+    if os.path.exists(target):
+        if overwrite:
+            os.remove(target)
+        else:
+            return
+
+    proxy = os.getenv(what+'_proxy')
+    if proxy : 
+        proxy_support = ProxyHandler({what:proxy})
+        opener = build_opener(proxy_support)
+        install_opener(opener)
+
+    #print 'openning',url
+
+    try:
+        res = urlopen(url)
+    except HTTPError, e:
+        print e.__class__, e 
+        raise IOError
+    except URLError, e:
+        print e.__class__, e 
+        raise IOError
+
+
+    targetfp = open(target,"w")
+    shutil.copyfileobj(res,targetfp)
+
+    return
+
+def uriparse(uri):
+    '''
+    Split the URI scheme://hostname/path into [scheme,hostname,path]
+    '''
+    colon = uri.find(':')
+    slashslash = colon + 3
+    slash = uri.find('/',slashslash)
+    #print colon, slashslash, slash
+    return [uri[:colon], uri[slashslash:slash], uri[slash:]]
+
+def get(url,target,overwrite=False):
+    '''
+    Get the file or directory tree at the given URL and place it at
+    the given target path.  If overwrite is True and target is
+    preexisting it will be overwritten (updated).
+
+    The URL is expected to be in a standard form:
+
+    SCHEME://HOSTNAME/PATH
+
+    The following URL schemes are supported:
+
+    http: - the file given in PATH via HTTP
+
+    ftp: - the file given in PATH via anonymous FTP
+
+    git+TRANSPORT: git-clone a repository.  TRANSPORT can be http,
+    rsync, ssh or empty to use native git protocol (the '+' can be
+    omitted).  See git-clone(1) for details.  If overwriting a
+    git-pull is done.
+
+    svn+TRANSPORT: - perform "svn co" using the remaining URL with
+    'svn+' removed.  If overwritting, an "svn update" is done.
+
+    '''
+
+    urlp = uriparse(url)
+    #print urlp
+    if urlp[0] == 'http' or urlp[0] == 'ftp':
+        get_http_ftp(urlp[0],url,target,overwrite)
+
+    pass
+
+if '__main__' == __name__:
+    import sys
+    url=sys.argv[1]
+    try:
+        target=sys.argv[2]
+    except IndexError:
+        target='file.out'
+    try:
+        overwrite=bool(sys.argv[3])
+    except IndexError:
+        overwrite=False
+    get(url,target,overwrite)
