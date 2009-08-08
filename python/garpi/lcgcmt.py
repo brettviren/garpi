ececa2cf2a0ce3b992aea71776ed6f80309afd73
Brett Viren <bv@bnl.gov>
2009-08-08 12:36:05 -0400

    Don't set CMTCONFIG in setup and add method to find biulder directory given package.

--- python/garpi/lcgcmt.py
+++ python/garpi/lcgcmt.py
@@ -10,34 +10,27 @@ class Lcgcmt(Project):
         Project.__init__(self,"lcgcmt")
         return
 
-    def project_externals(self,project):
-        '''Determine external dependencies of the given project.  The
-        project is assumed to be downloaded.  Returns an ordered
-        list.'''
-        return []
-
-
     def cmtconfig(self):
-        'Return the auto-determined CMTCONFIG value.'
+        'Return an auto-determined CMTCONFIG value.'
         import cmt
         rel_path = os.path.join(self.proj_dir(),self.rel_pkg(),'cmt')
         extra_env = cmt.env(rel_path)
 
         sets = cmt.sets(extra_env=extra_env,dir=rel_path)
         try:
-            return sets['CMTCONFIG']
+            return sets['CMTCONFIG'].strip()
         except KeyError:
             pass
 
         from exception import CommandFailure
         for macro in ['host-cmtconfig','LCG_platform']:
             try:
-                return cmt.macro(macro,extra_env=extra_env,dir=rel_path)
+                return cmt.macro(macro,extra_env=extra_env,dir=rel_path).strip()
             except CommandFailure:
                 pass
             continue
 
-        return 'unsupported-configuration'
+        return None
 
     def init_project(self,deps=[]):
         'Initialize the LCGCMT project'
@@ -45,20 +38,37 @@ class Lcgcmt(Project):
         setupdir = fs.setup()
         fs.assure(setupdir)
 
+        base = fs.setup()
+
         # Bourne Shell
         sh = open(os.path.join(base,'10_lcgcmt.sh'),'w')
         sh.write('''#!/bin/sh
 CMTPROJECTPATH=%s
-CMTCONFIG=%s
-export CMTPROJECTPATH,CMTCONFIG
-        '''%(fs.projects(),self.cmtconfig()))
+export CMTPROJECTPATH
+        '''%fs.projects())
         sh.close()
 
         # C(rappy) Shell
         csh = open(os.path.join(base,'10_lcgcmt.csh'),'w')
         csh.write('''#!/bin/csh
 setenv CMTPROJECTPATH %s
-setenv CMTCONFIG %s
-        '''%(fs.projects(),self.cmtconfig()))
+        '''%fs.projects())
         csh.close()
         return
+
+    def builder_directory(self,pkgname):
+        builders = os.path.join(self.proj_dir(),'LCG_Builders')
+        for thing in os.listdir(builders):
+            path = os.path.join(builders,thing)
+            if not os.path.isdir(path):
+                #print 'Not a dir: "%s"'%path
+                continue
+            if thing.lower() == pkgname.lower():
+                return path
+            #print 'Not matching "%s" != %s'%(thing,pkgname)
+            continue
+        return None
+
+    def build_package(self,pkg):
+        return
+
