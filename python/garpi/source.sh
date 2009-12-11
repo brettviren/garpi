c911ee7af9e6e7adc4ba21ba47daaf581d339ab6
Brett Viren <bvlbne@daya0001.rcf.bnl.gov>
2009-12-11 14:36:03 -0500

    Exit with error if given bad inputs.

--- python/garpi/source.sh
+++ python/garpi/source.sh
@@ -4,4 +4,14 @@ file=$1
 shift
 delim=$1
 
+if [ -z "$file" ] ; then
+    echo "No file given to source"
+    exit 1
+fi
+
+if [ ! -f $file ] ; then
+    echo "No such file: $file"
+    exit 1
+fi
+
 . $file && echo $delim && env
